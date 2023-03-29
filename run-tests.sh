493ddc4a8279cde3bee0db1d04fd7d4838d51ab9
Jan Tojnar <jtojnar@gmail.com>
2023-03-29 04:44:34 +0200

    ci: Fix
    After recent Nix or git bump, the build would fail due to git failing with:
    
        The following paths are ignored by one of your .gitignore files:
        tests/grav/flake.lock

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,11 @@
 set -x -o errexit
 
 cd tests/grav
+
+# Work around nix build failing due to git failing with
+#     The following paths are ignored by one of your .gitignore files:
+#     tests/grav/flake.lock
+nix flake lock || true
+git add --intent-to-add flake.lock -f
+
 nix build .#grav -L
