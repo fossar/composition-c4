4d1e0346061c87a2514a350863cab78a0bf9181b
Jan Tojnar <jtojnar@gmail.com>
2023-05-19 18:44:01 +0200

    tests: Do not create lockfile in the first place

--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
 set -x -o errexit
 
-cd tests
-
-# Nix locks path inputs, preventing us from comitting it.
-# Remove a potentially stale lockfile.
-# https://github.com/NixOS/nix/issues/3978
-rm -f flake.lock
-
-# Work around nix build failing due to git failing with
-#     The following paths are ignored by one of your .gitignore files:
-#     tests/flake.lock
-nix flake lock || true
-git add --intent-to-add flake.lock -f
-
-nix build .#grav -L
-
-# Clean up the produced lockfile.
-git reset flake.lock
-rm flake.lock
+nix build -L --no-write-lock-file ./tests#grav
