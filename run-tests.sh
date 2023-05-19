274364d4cb8b10f2644253a6b96e8be8bbb09452
Jan Tojnar <jtojnar@gmail.com>
2023-05-19 16:54:50 +0200

    tests: Clean flake.lock files
    Otherwise the top-level flake will be locked and changes will not be reflected.

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,11 @@ set -x -o errexit
 
 cd tests
 
+# Nix locks path inputs, preventing us from comitting it.
+# Remove a potentially stale lockfile.
+# https://github.com/NixOS/nix/issues/3978
+rm -f flake.lock
+
 # Work around nix build failing due to git failing with
 #     The following paths are ignored by one of your .gitignore files:
 #     tests/flake.lock
@@ -10,3 +15,7 @@ nix flake lock || true
 git add --intent-to-add flake.lock -f
 
 nix build .#grav -L
+
+# Clean up the produced lockfile.
+git reset flake.lock
+rm flake.lock
